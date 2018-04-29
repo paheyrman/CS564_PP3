4d970c4162314a5859d84db7664800cc0c550712
Benjamin C Charles <benjamincraigcharles@gmail.com>
2018-04-29 16:51:36 -0500

    Add files via upload

--- create_auctionbase/runParser.sh
+++ create_auctionbase/runParser.sh
@@ -1,4 +1,4 @@
-python skeleton_parser.py /usr/class/cs145/project/ebay_data/items-*.json
+python skeleton_parser.py /Users/bencharles/Desktop/Spring\ 2018/CS\ 564/PP3/auctionbase/ebay_data/items-*.json
 
 sort -u items.dat -o itemsUnique.dat
 sort -u categories.dat -o categoriesUnique.dat
