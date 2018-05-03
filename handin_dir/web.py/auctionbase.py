3fab6bfd4085e6a3f0604d2e5d2b84d975fb54fa
nbkrause <nbkrause@wisc.edu>
2018-05-03 03:45:09 -0500

    Files commented and handin directory created.

---
+++ handin_dir/web.py/auctionbase.py
@@ -0,0 +1,293 @@
+###########################################################################################
+# Authors: Benjamin Charles 9071177506
+#          Paul Heyrman     9071315551
+#          Noah Krause      9071587712
+###########################################################################################
+# Description: This is the main front end to our AuctionDatabase website. This website 
+#              allows users to search for and make bids on ebay items stored on the 
+#              database. This file contains methods to view the current time, set the
+#              current time, search the database, and bid on items within the database.
+#              auctionbase.py calls functions in sqlitedb.py to execute queries related to
+#              this functionality.
+###########################################################################################
+
+
+#!/usr/bin/env python
+
+import sys; sys.path.insert(0, 'lib')
+import os                            
+
+import web
+import sqlitedb
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+
+###########################################################################################
+##########################DO NOT CHANGE ANYTHING ABOVE THIS LINE!##########################
+###########################################################################################
+
+######################BEGIN HELPER METHODS######################
+
+# helper method to convert times from database (which will return a string)
+# into datetime objects. This will allow you to compare times correctly (using
+# ==, !=, <, >, etc.) instead of lexicographically as strings.
+
+# Sample use:
+# current_time = string_to_time(sqlitedb.getTime())
+
+def string_to_time(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+# helper method to render a template in the templates/ directory
+#
+# `template_name': name of template file to render
+#
+# `**context': a dictionary of variable names mapped to values
+# that is passed to Jinja2's templating engine
+#
+# See curr_time's `GET' method for sample usage
+#
+# WARNING: DO NOT CHANGE THIS METHOD
+def render_template(template_name, **context):
+    extensions = context.pop('extensions', [])
+    globals = context.pop('globals', {})
+
+    jinja_env = Environment(autoescape=True,
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+            extensions=extensions,
+            )
+    jinja_env.globals.update(globals)
+
+    web.header('Content-Type','text/html; charset=utf-8', unique=True)
+
+    return jinja_env.get_template(template_name).render(context)
+
+#####################END HELPER METHODS#####################
+
+urls = ('/currtime', 'curr_time',
+        '/selecttime', 'select_time',
+        '/add_bid', 'place_bid',
+        '/appbase', 'appbase',
+        '/search', 'auction_search',
+        '/items', 'selected_item',
+        '/', 'auction_search')
+
+class selected_item:
+
+     def GET(self):
+        post_params = web.input()
+        itemID = post_params['id']
+        item = sqlitedb.getItemById(itemID)
+        categories = sqlitedb.getCategoryById(itemID)
+        bids = sqlitedb.getBidById(itemID)
+        ended = False
+        hasBuyPrice = False
+
+        if item.Started <= sqlitedb.getTime() and item.Ends >= sqlitedb.getTime(): status = 'Currently Open'
+        elif item.Started > sqlitedb.getTime(): status = 'Has Not Started'
+        else: status = 'Closed'
+        if item.Number_of_Bids == 0: 
+            noBids = True
+            winner = ""
+        else: 
+            noBids = False
+            winner = sqlitedb.getWinnerById(itemID).UserID
+        
+        if item.Buy_Price is not None:
+            if status == 'Closed' or float(item.Currently) >= float(item.Buy_Price):
+                status = 'Closed'
+                ended = True
+                hasBuyPrice = True
+                buyPrice = item.Buy_Price
+        elif status == 'Closed':
+            ended = True
+            buyPrice = ""
+        
+        return render_template('items.html', id = itemID, bids = bids, Name = item.Name, Category = categories.Category, Ends = item.Ends, Started = item.Started, Number_of_Bids = item.Number_of_Bids, Seller = item.Seller_UserID, Description = item.Description, Currently = item.Currently, noBids = noBids, ended = ended, Status = status, Winner = winner, buyPrice = buyPrice, hasBuyPrice = hasBuyPrice)
+
+class curr_time:
+    # A simple GET request, to '/currtime'
+    #
+    # Notice that we pass in `current_time' to our `render_template' call
+    # in order to have its value displayed on the web page
+    def GET(self):
+        current_time = sqlitedb.getTime()
+        return render_template('curr_time.html', time = current_time)
+
+
+class select_time:
+    # Another GET request, this time to the URL '/selecttime'
+    def GET(self):
+        return render_template('select_time.html')
+
+    # A POST request
+    #
+    # You can fetch the parameters passed to the URL
+    # by calling `web.input()' for **both** POST requests
+    # and GET requests
+    def POST(self):
+        post_params = web.input()
+        MM = post_params['MM']
+        dd = post_params['dd']
+        yyyy = post_params['yyyy']
+        HH = post_params['HH']
+        mm = post_params['mm']
+        ss = post_params['ss']
+        enter_name = post_params['entername']
+
+        selected_time = '%s-%s-%s %s:%s:%s' % (yyyy, MM, dd, HH, mm, ss)
+        update_message = '(Hello, %s. Previously selected time was: %s.)' % (enter_name, selected_time)
+        try: sqlitedb.update_auction_time(selected_time)
+        except Exception as timeEx: 
+            update_message = 'Unable to update time, time value selected is not valid'
+            print(str(timeEx))
+        return render_template('select_time.html', message = update_message)
+
+
+class auction_search:
+    # GET request to the URL search.html that supports the search functions.
+    def GET(self):
+        return render_template('search.html')
+
+    # Fetches the search parameters entered by the user and passes them to sqlitedb.py.
+    def POST(self):
+        post_params = web.input()
+        itemID = post_params['itemID']
+        userID = post_params['userID']
+        category = post_params['category']
+        description = post_params['description']
+        minPrice = post_params['minPrice']
+        maxPrice = post_params['maxPrice']
+        status = post_params['status']
+
+        val = sqlitedb.auction_search(itemID, userID, category, description, minPrice, maxPrice, status)
+        return render_template('search.html', search_result = val)
+###########################################################################################
+##########################DO NOT CHANGE ANYTHING BELOW THIS LINE!##########################
+###########################################################################################
+
+if __name__ == '__main__':
+    web.internalerror = web.debugerror
+    app = web.application(urls, globals())
+    app.add_processor(web.loadhook(sqlitedb.enforceForeignKey))
+    app.run()
+
+
+
+class curr_time:
+    # A simple GET request, to '/currtime'
+    #
+    # Notice that we pass in `current_time' to our `render_template' call
+    # in order to have its value displayed on the web page
+    def GET(self):
+        current_time = sqlitedb.getTime()
+        return render_template('curr_time.html', time = current_time)
+
+
+class select_time:
+    # Another GET request, this time to the URL '/selecttime'
+    def GET(self):
+        return render_template('select_time.html')
+
+    # A POST request
+    #
+    # You can fetch the parameters passed to the URL
+    # by calling `web.input()' for **both** POST requests
+    # and GET requests
+    def POST(self):
+        post_params = web.input()
+        MM = post_params['MM']
+        dd = post_params['dd']
+        yyyy = post_params['yyyy']
+        HH = post_params['HH']
+        mm = post_params['mm']
+        ss = post_params['ss']
+        enter_name = post_params['entername']
+
+        selected_time = '%s-%s-%s %s:%s:%s' % (yyyy, MM, dd, HH, mm, ss)
+        update_message = '(Hello, %s. Previously selected time was: %s.)' % (enter_name, sqlitedb.getTime())
+        try: sqlitedb.update_auction_time(selected_time)
+        except Exception as timeEx: 
+            update_message = 'Unable to update time, time value selected is not valid'
+            print(str(timeEx))
+        return render_template('select_time.html', message = update_message)
+
+
+class place_bid:
+
+    def GET(self):
+        return render_template('add_bid.html')
+
+    def POST(self):
+        post_params = web.input()
+        itemID = post_params['itemID']
+        userID = post_params['userID']
+        Amount = post_params['price']
+
+        #Verify the correct search criteria was entered
+        if itemID == '' or userID == '' or Amount == '':
+            #If not entered, prompt user to conduct a new search with the appropriate values entered
+            return render_template('add_bid.html', message = 'Invalid search criteria, ensure ItemID, UserID, Amount are valid.')
+        else:
+            #If entered, obtain the item and user information specified by the search criteria
+            curr_item = sqlitedb.getItemById(itemID)
+            curr_user = sqlitedb.getUserById(userID)
+        #Verify the item is valid in the database
+        if curr_item is None:
+            return render_template('add_bid.html', message = 'Unable to find item, ItemID is invalid.')
+        #Verify the user is valid in the database
+        elif curr_user is None:
+            return render_template('add_bid.html', message = 'Unable to find user, UserID is invalid.')
+        #Vefify the user is not bidding on an item they are also selling
+        elif curr_user.UserID == curr_item.Seller_UserID:
+            return render_template('add_bid.html', message = 'Unable to place bid, users may not bid on items they are selling.')
+        #Verify the auction has not concluded
+        elif string_to_time(sqlitedb.getTime()) >= string_to_time(curr_item.Ends):
+            return render_template('add_bid.html', message = 'Unable to place bid, auction has ended.')
+        #Verify the auction has begun
+        elif string_to_time(sqlitedb.getTime()) < string_to_time(curr_item.Started):
+            return render_template('add_bid.html', message = 'Unable to place bid, auction has not started.')
+        #Verify a valid bid amount was entered
+        elif float(Amount) < 0 or float(Amount) <= float(curr_item.First_Bid) or float(Amount) <= float(curr_item.Currently):
+            return render_template('add_bid.html', message = 'Unable to place bid, amount specified is invalid.')
+        #Verify the item has a buy price, if buy price is met close the auction
+        if curr_item.Buy_Price is not None:
+            if float(Amount) >= float(curr_item.Buy_Price):
+                successful_purchase = 'Congratulations! You have purchased item: %s for amount: %s.' % (curr_item.Name,Amount)
+                return render_template('add_bid.html', message = successful_purchase, add_result = sqlitedb.close_auction(itemID,userID,Amount))
+        #Verified no errors exist, place a new bid on the item for the amount specified
+        successful_bid = 'Congratulations! You have placed a bid on item: %s of amount: %s.' % (curr_item.Name,Amount)
+        return render_template('add_bid.html', message = successful_bid, add_result = sqlitedb.new_bid(itemID,userID,Amount))
+
+
+class auction_search:
+
+    def GET(self):
+        return render_template('search.html')
+
+    def POST(self):
+        post_params = web.input()
+        itemID = post_params['itemID']
+        userID = post_params['userID']
+        category = post_params['category']
+        description = post_params['description']
+        minPrice = post_params['minPrice']
+        maxPrice = post_params['maxPrice']
+        status = post_params['status']
+        # Checks for valid search criteria displays error message if proper fields are 
+        # not populated and passes the input to sqlitedb.py if they are.
+        if itemID == '' and userID == '' and category == '' and description == '' and minPrice == '' and maxPrice == '':
+            return render_template('search.html', message = 'Unable to query the database with no search criteria, please enter one of the following fields and try again.')
+        else:
+            val = sqlitedb.auction_search(itemID, userID, category, description, minPrice, maxPrice, status)
+            return render_template('search.html', search_result = val)
+
+###########################################################################################
+##########################DO NOT CHANGE ANYTHING BELOW THIS LINE!##########################
+###########################################################################################
+
+if __name__ == '__main__':
+    web.internalerror = web.debugerror
+    app = web.application(urls, globals())
+    app.add_processor(web.loadhook(sqlitedb.enforceForeignKey))
+    app.run()
---
+++ handin_dir/web.py/sqlitedb.py
@@ -0,0 +1,136 @@
+###########################################################################################
+# Authors: Benjamin Charles 9071177506
+#          Paul Heyrman     9071315551
+#          Noah Krause      9071587712
+###########################################################################################
+# Description: This file provides the interactions between the front-end website and the
+#              database. It calls functions to execute SQLite queries against the database
+#              with parameters recieved from auctionbase.py and returns the results to be
+#              displayed in HTML. 
+###########################################################################################
+
+
+import web
+
+db = web.database(dbn = 'sqlite', db = 'AuctionBase')
+
+######################BEGIN HELPER METHODS######################
+
+# Enforce foreign key constraints
+def enforceForeignKey():
+    db.query('PRAGMA foreign_keys = ON')
+
+# initiates a transaction on the database
+def transaction():
+    return db.transaction()
+
+# Returns the current time from your database
+def getTime():
+    query_string = 'select Time from CurrentTime'
+    results = query(query_string)
+    return results[0].Time
+
+# Returns a single item specified by the Item's ID in the database
+def getItemById(item_id):
+    query_string = 'select * from Items where ItemID = $itemID'
+    result = query(query_string, {'itemID': item_id})
+    try: return result[0]
+    except IndexError: return None
+
+# Returns all categories of the item identified by item_id
+def getCategoryById(item_id):
+    query_string = 'select group_concat(Category,", ") as Category from Categories where ItemID = $itemID'
+    result = query(query_string, {'itemID': item_id})
+    try: return result[0]
+    except IndexError: return None
+
+# Returns information for bids on the item identified by item_id
+def getBidById(item_id):
+    query_string = 'select UserID as "ID of Bidder", Time as "Time of Bid", Amount as "Price of Bid" from Bids where ItemID = $itemID'
+    result = query(query_string, {'itemID': item_id})
+    try: return result
+    except IndexError: return None
+
+# Returns all information from the winning bid for the item_id item
+def getWinnerById(item_id):
+    query_string = 'select * from Bids where ItemID = $itemID and Amount = (select Max(Amount) from Bids where ItemID = $itemID)'
+    result = query(query_string, {'itemID': item_id})
+    try: return result[0]
+    except IndexError: return None
+
+# Returns a single user specified by the User's ID in the database 
+def getUserById(user_id):
+    query_string = 'select * from Users where UserID = $userID'
+    result = query(query_string, {'userID': user_id})
+    try: return result[0]
+    except IndexError: return None
+
+
+# Wrapper method around web.py's db.query method
+def query(query_string, vars = {}):
+    return list(db.query(query_string, vars))
+
+#####################END HELPER METHODS#####################
+
+#TODO: additional methods to interact with your database,
+# e.g. to update the current time
+def update_auction_time(curr_time):
+    t = transaction()
+    try: db.update('CurrentTime', where = 'Time = $cTime', vars = { 'cTime': getTime() }, Time = curr_time)
+    except Exception as timeEx: 
+        t.rollback() 
+        print(str(timeEx))
+        raise Exception
+    else: t.commit()
+
+#Place a new bid on the specified item for the specifed amount by the user in question
+def new_bid(curr_item, curr_user, curr_amount):
+    t = transaction()
+    try: db.insert('Bids', UserID = curr_user, ItemID = curr_item, Amount = curr_amount, Time = getTime())
+    except Exception as bidEx: 
+        t.rollback()
+        return False
+    else: 
+        t.commit()
+        return True
+
+#Close the auction if the item's buy price has been met, update the item's feilds to reflect the change
+def close_auction(curr_item, curr_user, curr_amount):
+    t = transaction()
+    try: db.update('Items', where = 'ItemID = $cItem', vars = { 'cItem': curr_item }, ItemID = curr_item, Ends = getTime(),
+                   Currently = curr_amount)
+    except Exception as bidEx: 
+        t.rollback()
+        return False
+    else: 
+        t.commit()
+        return True
+
+# Seach the auction database for items that match the query paramters passed
+# by auctionbase.py
+def auction_search(itemID, userID, category, description, minPrice, maxPrice, status):
+
+    if description is None: description = '%%'
+    else: description = '%' + description + '%'
+    if minPrice == '': minPrice = 0;
+    else: minPrice = int(minPrice)
+    if maxPrice == '': maxPrice = 999999999999999999;
+    else: maxPrice = int(maxPrice)
+
+    if status == 'open':
+        query_string = 'select *, group_concat(category,", ") as Category from Items, Categories, CurrentTime where (Categories.ItemID = Items.ItemID) AND (IFNULL($category, "") = "" OR $category = Categories.category) AND (IFNULL($itemID, "") = "" OR $itemID = Items.ItemID) AND (IFNULL($userID, "") = "" OR $userID = Items.Seller_UserID) AND (Items.Description LIKE $description) AND (IFNULL(Items.Currently, Items.First_Bid) >= $minPrice) AND (IFNULL(Items.Currently, Items.First_Bid) <= $maxPrice) AND (Items.Started <= CurrentTime.Time AND Items.Ends >= CurrentTime.Time) group by Items.ItemID'
+    if status == 'close':
+        query_string = 'select *, group_concat(category,", ") as Category from Items, Categories, CurrentTime where (Categories.ItemID = Items.ItemID) AND (IFNULL($category, "") = "" OR $category = Categories.category) AND (IFNULL($itemID, "") = "" OR $itemID = Items.ItemID) AND (IFNULL($userID, "") = "" OR $userID = Items.Seller_UserID) AND (Items.Description LIKE $description) AND (IFNULL(Items.Currently, Items.First_Bid) >= $minPrice) AND (IFNULL(Items.Currently, Items.First_Bid) <= $maxPrice) AND (Items.Ends < CurrentTime.Time) group by Items.ItemID'
+    if status == 'notStarted': 
+        query_string = 'select *, group_concat(category,", ") as Category from Items, Categories, CurrentTime where (Categories.ItemID = Items.ItemID) AND (IFNULL($category, "") = "" OR $category = Categories.category) AND (IFNULL($itemID, "") = "" OR $itemID = Items.ItemID) AND (IFNULL($userID, "") = "" OR $userID = Items.Seller_UserID) AND (Items.Description LIKE $description) AND (IFNULL(Items.Currently, Items.First_Bid) >= $minPrice) AND (IFNULL(Items.Currently, Items.First_Bid) <= $maxPrice) AND (Items.Started > CurrentTime.Time) group by Items.ItemID'
+    if status == 'all':
+        query_string = 'select *, group_concat(category,", ") as Category from Items, Categories, CurrentTime where (Categories.ItemID = Items.ItemID) AND (IFNULL($category, "") = "" OR $category = Categories.category) AND (IFNULL($itemID, "") = "" OR $itemID = Items.ItemID) AND (IFNULL($userID, "") = "" OR $userID = Items.Seller_UserID) AND (Items.Description LIKE $description) AND (IFNULL(Items.Currently, Items.First_Bid) >= $minPrice) AND (IFNULL(Items.Currently, Items.First_Bid) <= $maxPrice) group by Items.ItemID'
+    
+    result = query(query_string, {'category': category, 'itemID': itemID, 'userID': userID, 'description': description, 'minPrice': minPrice, 'maxPrice': maxPrice});
+    try: return result
+    except IndexError: return None
+
+
+
+
+
