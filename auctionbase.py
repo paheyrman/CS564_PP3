95ab3b3997bb4a57155ecb3ef5777991e3294207
Benjamin C Charles <benjamincraigcharles@gmail.com>
2018-05-03 10:08:39 -0500

    Add files via upload

--- auctionbase.py
+++ auctionbase.py
@@ -80,29 +80,25 @@ class selected_item:
         bids = sqlitedb.getBidById(itemID)
         ended = False
         hasBuyPrice = False
+        buyPrice = ""
+        winner = ""
 
         if item.Started <= sqlitedb.getTime() and item.Ends >= sqlitedb.getTime(): status = 'Currently Open'
         elif item.Started > sqlitedb.getTime(): status = 'Has Not Started'
         else: status = 'Closed'
-        if item.Number_of_Bids == 0: 
-            noBids = True
-            winner = ""
+        if item.Number_of_Bids == 0: noBids = True
         else: 
             noBids = False
             winner = sqlitedb.getWinnerById(itemID).UserID
         
         if item.Buy_Price is not None:
+            hasBuyPrice = True
+            buyPrice = item.Buy_Price
             if status == 'Closed' or float(item.Currently) >= float(item.Buy_Price):
                 status = 'Closed'
                 ended = True
-                hasBuyPrice = True
-                buyPrice = item.Buy_Price
-            else:
-                hasBuyPrice = True
-                buyPrice = item.Buy_Price
         elif status == 'Closed':
             ended = True
-            buyPrice = ""
         
         return render_template('items.html', id = itemID, bids = bids, Name = item.Name, Category = categories.Category, Ends = item.Ends, Started = item.Started, Number_of_Bids = item.Number_of_Bids, Seller = item.Seller_UserID, Description = item.Description, Currently = item.Currently, noBids = noBids, ended = ended, Status = status, Winner = winner, buyPrice = buyPrice, hasBuyPrice = hasBuyPrice)
 
